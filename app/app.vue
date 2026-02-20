cf11e7cdc09bffbaa86ce4ad6d11d828c6e1111d
mfcworks <mfcworks@outlook.jp>
2026-02-20 16:38:44 +0900

    Create test page

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,13 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <h1>Hello, Cloudflare Pages!</h1>
+    <p class="nuxt-color">This is a test site for Cloudflare Pages with Nuxt.</p>
   </div>
 </template>
+
+<style scoped>
+.nuxt-color {
+  color: #00DC82;
+  font-weight: bold;
+}
+</style>
